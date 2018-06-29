fded95b2adfcfdb20896bb5ab8c13e35affe7338
Varun Sukheja <varun@shoppinpal.com>
2018-06-29 20:18:47 +0530

    - Added after block to clear DB after running all the tests. - Added steps to clear DB in readme file - Added test to check POST contact.

--- tests/api/contact.test.js
+++ tests/api/contact.test.js
@@ -10,32 +10,81 @@ let server = require('../../server/server');
 let should = chai.should();
 chai.use(chaiHttp);
 
-// Model to test
-let Contact = require('../../common/models/contact');
-
 describe('Contacts', () => {
-  beforeEach((done) => {
-    // Before each test we empty the database
-    // Contact.remove({}, (err) => {
-    //   done();
-    // });
-    done();
-  });
-
   /*
-  * Test the /GET route
+  * Test the /POST route
   */
-  describe('/GET contact', () => {
-    it('it should GET all the contacts', (done) => {
+  describe('/POST contact', () => {
+    it('it should POST a new contact', (done) => {
+      let contact = {
+        firstName: 'varun',
+        lastName: 'sukheja',
+        phoneNumber: 1234567890,
+        email: 'sukheja.varun@gmail.com',
+      };
+      let res = {
+        firstName: 'varun',
+        lastName: 'sukheja',
+        phoneNumber: 1234567890,
+        email: 'sukheja.varun@gmail.com',
+        isActive: true,
+        country: 'India',
+        countryCode: '+91',
+        id: 1};
       chai.request(server)
-        .get('/api/Contacts')
+        .post('/api/Contacts')
+        .send(contact)
         .then(function(response) {
           response.should.have.status(200);
+          response.body.should.eql(res);
           done();
         })
         .catch(function(error) {
           done(error);
         });
+    });
+  });
+  /*
+  * Test the /GET route
+  */
+  describe('/GET contact', () => {
+    it('it should GET all the contacts', (done) => {
+      let res = [{firstName: 'varun',
+        lastName: 'sukheja',
+        phoneNumber: 1234567890,
+        email: 'sukheja.varun@gmail.com',
+        isActive: true,
+        country: 'India',
+        countryCode: '+91',
+        id: 1}];
+      chai.request(server)
+        .get('/api/Contacts')
+        .then(function(response) {
+          response.should.have.status(200);
+          response.body.should.have.length(1);
+          response.body.should.eql(res);
+          done();
+        })
+        .catch(function(error) {
+          done(error);
+        });
+    });
+  });
+
+  after(() => {
+    // After all test we empty the database
+    let fs = require('fs');
+    let fileToRemove = 'data/test-data.json';
+    fs.unlink(fileToRemove, function(err) {
+      if (err && err.code == 'ENOENT') {
+        // file doesn't exist
+        console.info('File does not exist');
+      } else if (err) {
+        // other errors, e.g. maybe we don't have enough permission
+        console.error('Error occurred while trying to remove DB file');
+      } else {
+        console.info('removed DB file');
+      }
     });
   });
 });
