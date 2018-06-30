1aeb276f1ee5eed526b8134f8b1a2db2b93669c9
Varun Sukheja <varun@shoppinpal.com>
2018-07-01 03:09:42 +0530

    - created thumbnail component - added Material Card to design Contact Thumbnail - added SCSS to change material design

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ThumbnailComponent } from './thumbnail/thumbnail.component';
 import {MaterialModule} from './material';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThumbnailComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/material.ts
+++ client/src/app/material.ts
@@ -1,11 +1,13 @@
 import {NgModule} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 
 
 @NgModule({
-  imports: [MatCardModule, MatIconModule],
-  exports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule],
+  exports: [MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule],
 })
 export class MaterialModule {
 }
