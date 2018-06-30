6ca5d2307d40d1e90b680678a7fff7141ffbf3ba
Varun Sukheja <varun@shoppinpal.com>
2018-06-30 23:38:08 +0530

    - created MaterialModule which handles material dependencies - added steps for creating MaterialModule in readme file

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from './material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
