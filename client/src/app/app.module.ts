fc162b3e3c2e6ceaa79210ae4119fea235399efc
Varun Sukheja <varun@shoppinpal.com>
2018-07-02 01:16:40 +0530

    created component list-view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { ThumbnailComponent } from './thumbnail/thumbnail.component';
 import {MaterialModule} from './material';
 import { ThumbnailListComponent } from './thumbnail-list/thumbnail-list.component';
+import { ListViewComponent } from './list-view/list-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThumbnailComponent,
-    ThumbnailListComponent
+    ThumbnailListComponent,
+    ListViewComponent
   ],
   imports: [
     BrowserModule,
