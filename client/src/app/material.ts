ed99356b148348dfea75f45a7be96f8e1bdbdd7b
Varun Sukheja <varun@shoppinpal.com>
2018-07-02 00:40:30 +0530

    added navbar in app.component

--- client/src/app/material.ts
+++ client/src/app/material.ts
@@ -3,11 +3,13 @@ import {MatCardModule} from '@angular/material/card';
 import {MatListModule} from '@angular/material/list';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
-  imports: [MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule],
-  exports: [MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule],
+  imports: [MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule, MatToolbarModule, MatButtonModule],
+  exports: [MatCardModule, MatListModule, MatIconModule, MatSlideToggleModule, MatToolbarModule, MatButtonModule],
 })
 export class MaterialModule {
 }
