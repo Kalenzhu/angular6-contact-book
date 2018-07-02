4b4afcb0d74783a3b4aa4506da27032304d50bdd
Varun Sukheja <varun@shoppinpal.com>
2018-07-02 20:01:06 +0530

    - added error handler

--- client/src/app/contact.service.ts
+++ client/src/app/contact.service.ts
@@ -2,8 +2,8 @@ import {Injectable} from '@angular/core';
 import {Contact} from './contact';
 import {CONTACTS} from './mock-contacts';
 import {Observable, of} from 'rxjs/index';
-
 import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {catchError, map, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +16,10 @@ export class ContactService {
   }
 
   getContacts(): Observable<Contact[]> {
-    return this.http.get<Contact[]>(this.contactUrl);
+    return this.http.get<Contact[]>(this.contactUrl)
+      .pipe(
+        catchError(this.handleError('getContacts', []))
+      );
   }
 
   getContact(id: number): Contact {
@@ -40,5 +43,25 @@ export class ContactService {
     const index = CONTACTS.indexOf(contact);
     CONTACTS.splice(index, 1);
     alert('Contact Deleted!');
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      alert(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
